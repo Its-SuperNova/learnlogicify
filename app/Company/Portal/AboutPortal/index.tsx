1340ebdfe84938fee9c5400825775155df8f4da8
SuperNova <its.ashwin.23@gmail.com>
2024-08-29 09:38:42 +0530

    Portal - aboutPortal section completed

--- app/Company/Portal/AboutPortal/index.tsx
+++ app/Company/Portal/AboutPortal/index.tsx
@@ -1,11 +1,19 @@
+"use client";
 import React from "react";
 import styles from "./Styles.module.css";
 import Video from "./components/video section"; // Import the Video component
+import CodeIcon from "@/public/SVG/icons/code.svg";
+import BarIcon from "@/public/SVG/icons/bar.svg";
+import GraphIcon from "@/public/SVG/icons/graph.svg";
+import { GiCircle } from "react-icons/gi";
+import { GiSquare } from "react-icons/gi";
+import Lottie from "lottie-react";
+import GearAnimation from "@/public/SVG/icons/gear.json"; // Import the Lottie JSON file
 
 const AboutPortal = () => {
   return (
     <main className={styles.container}>
-      <header>
+      <header className={styles.header}>
         <h2 className={styles.subtitle}>
           <span className={styles.animatedGradient}>
             Welcome to Your LearnLogicify LMS Portal
@@ -26,6 +34,39 @@ const AboutPortal = () => {
             muted={false}
           />
         </div>
+        <div className={styles.background}>
+          <img
+            src={CodeIcon.src}
+            alt="Code Icon"
+            className={`${styles.icon} ${styles.icon1}`}
+          />
+          <img
+            src={BarIcon.src}
+            alt="Bar Icon"
+            className={`${styles.icon} ${styles.icon2}`}
+          />
+          <img
+            src={GraphIcon.src}
+            alt="Graph Icon"
+            className={`${styles.icon} ${styles.icon3}`}
+          />
+          {/* Additional Icons */}
+          <GiCircle className={`${styles.icon} ${styles.icon4}`} />
+          <GiSquare className={`${styles.icon} ${styles.icon5}`} />
+          <div className={`${styles.icon} ${styles.icon6}`}>
+            <Lottie animationData={GearAnimation} loop={true} />
+          </div>
+        </div>
+      </section>
+      <section className={styles.desc}>
+        <p>
+          The LearnLogicify LMS Portal offers personalized dashboards,
+          interactive modules, and a built&hyphen;in compiler for hands-on
+          practice. Each course includes detailed notes and engaging animated
+          videos to simplify complex concepts. With comprehensive resources at
+          your fingertips, you&apos;ll be well-equipped to excel in your studies
+          and career.
+        </p>
       </section>
     </main>
   );
