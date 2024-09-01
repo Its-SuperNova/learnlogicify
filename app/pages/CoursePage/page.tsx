8a1593e45d2d49603ca5a77137489ac97f0f1c9b
SuperNova <its.ashwin.23@gmail.com>
2024-09-01 22:01:53 +0530

    hamburger sidebar added to course page

--- app/pages/CoursePage/page.tsx
+++ app/pages/CoursePage/page.tsx
@@ -3,11 +3,20 @@ import styles from "./styles.module.css";
 import Hero from "./components/Hero";
 import Header from "@/app/components/common/Header";
 import Syllabus from "./components/Syllabus";
-import CertificateSection from "../common/CertificateSection"
-import CertificateProcess from "./components/CertificationProcess"
+import CertificateSection from "../common/CertificateSection";
+import CertificateProcess from "./components/CertificationProcess";
+import dynamic from "next/dynamic";
+const ScrollHandler = dynamic(
+  () => import("@/app/components/Home/ScrollHandler"),
+  {
+    ssr: false,
+  }
+);
+
 const CoursePage = () => {
   return (
     <>
+      <ScrollHandler />
       <Header />
       <div className={styles.main}>
         <div className={styles.container}>
