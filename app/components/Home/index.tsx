c02dc23f52d4b81740bb5bd21dcc245528f9e22a
SuperNova <its.ashwin.23@gmail.com>
2024-09-05 13:01:41 +0530

    dark header completed successfully

--- app/components/Home/index.tsx
+++ app/components/Home/index.tsx
@@ -6,19 +6,13 @@ import ServiceGallery from "../common/ServiceGallery";
 import FeedBack from "./FeedBack";
 import Probanner from "../common/ProBanner";
 import styles from "./styles.module.css";
-import Header from "../common/Header";
+import Header from "../common/HeaderDark";
+import Footer from "../common/Footer";
 const HomePage: React.FC = () => {
   return (
     <main className={styles.main}>
       <Header />
-      <div className="hero-section">
-        <HeroPage />
-      </div>
-      <VideoSection />
-      <ServicesSection />
-      <ServiceGallery />
-      <FeedBack />
-      <Probanner />
+      <HeroPage />
     </main>
   );
 };
---
+++ app/components/common/HeaderDark/index.tsx
@@ -0,0 +1,31 @@
+// header/index.tsx
+"use client";
+import React, { useState } from "react";
+import styles from "./styles.module.css";
+import NavMenu from "./NavMenu";
+import sideNav from "../../hamburger/nav";
+import Logo from "../../images/logo/darkLogoheader";
+import LoginButton from "../buttons/login";
+import Link from "next/link";
+import MenuIcon from "../SVG/icons/hamburger";
+const Header = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.left}>
+          <Link href="/">
+            <Logo />
+          </Link>
+        </div>
+        <div className={styles.middle}>
+          <NavMenu />
+        </div>
+        <div className={styles.right}>
+          <div className={styles.login}>
+            <LoginButton />
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
