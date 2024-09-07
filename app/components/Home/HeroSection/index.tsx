b7d6a22d9b9addf6d8e9857607e48767c1ebd0f5
SuperNova <its.ashwin.23@gmail.com>
2024-09-07 22:28:10 +0530

    home hero-section completed

--- app/components/Home/HeroSection/index.tsx
+++ app/components/Home/HeroSection/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import styles from './styles.module.css'
+"use client";
+import React from "react";
+import styles from "./styles.module.css";
+
 const HeroHome = () => {
   return (
     <div className={styles.container}>
+      <div className={styles.subtitle}>
+        <h1 className={styles.animatedGradient2}>
+          Welcome to LearnLogicify Technologies
+        </h1>
+      </div>
       <div className={styles.title}>
-        <h1 className={styles.animatedGradient}>LearnLogicify</h1>
+        <h3 className={styles.animatedGradient}>
+          "Accelerate the Tech Career and build your Future Faster"
+        </h3>
+      </div>
+      <p className={styles.desc}>
+        Discover a wide range of Programming courses Tailored to enhance your
+        skills.
+      </p>
+      {/* Adding the button here */}
+      <div className={styles.btnContainer}>
+        <div className={styles.btn}>View Courses</div>
       </div>
     </div>
   );
-}
+};
 
-export default HeroHome
+export default HeroHome;
