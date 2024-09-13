6fceab539e0c6f76c7da0963b23287fd07d9be24
SuperNova <its.ashwin.23@gmail.com>
2024-09-13 15:05:28 +0530

    bug fixed

--- app/components/common/ServiceGallery/index.tsx
+++ app/components/common/ServiceGallery/index.tsx
@@ -1,20 +1,7 @@
-"use client";
-import { useInView } from "react-intersection-observer";
-import SlideUpWord from "../Animations/slideUpWord";
-import styles from "./styles.module.css";
+import React from "react";
 
-export default function ServiceGallery() {
-  const { ref: descriptionRef, inView: isInView } = useInView({
-    triggerOnce: false, // Re-trigger the animation when in view
-    threshold: 0.1, // Trigger when 10% of the section is visible
-  });
+const ServiceGallery = () => {
+  return <div>hello</div>;
+};
 
-  // Title for the ServiceGallery section with a line break after "Training"
-  const title = ["Look into our Training and ", "Service Gallery"];
-
-  return (
-    <div ref={descriptionRef} className={styles.serviceGallery}>
-      <SlideUpWord title={title} isInView={isInView} className={styles.title} />
-    </div>
-  );
-}
+export default ServiceGallery;
