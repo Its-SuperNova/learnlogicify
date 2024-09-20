00a791914f823094022d60ba07acfdab2b67350c
SuperNova <its.ashwin.23@gmail.com>
2024-09-20 20:36:35 +0530

    bug fixed succesfully

--- app/components/Home/sections/Testimonial/card/index.tsx
+++ app/components/Home/sections/Testimonial/card/index.tsx
@@ -8,10 +8,10 @@ const Card = () => {
         <img className={styles.profile} src="images/students/s1.jpg" alt="" />
         <div className={styles.content}>
           <p className={styles.feed}>
-            LearnLogicify is fantastic! The courses are well-structured, and the
-            platform is super easy to navigate. I've gained valuable tech
-            skills, and the hands-on exercises really boosted my confidence.
-            Highly recommend!
+            LearnLogicify is fantastic&#33; The courses are well&#45;structured,
+            and the platform is super easy to navigate. I&#39;ve gained valuable
+            tech skills, and the hands&#45;on exercises really boosted my
+            confidence. Highly recommend&#33;
           </p>
           <p className={styles.name}>Arun Kumar</p>
           <p className={styles.role}>Student</p>
