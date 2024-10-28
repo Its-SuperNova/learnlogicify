ccc9d4b08bc2317fa7e1a25df2a8c8300c2cad6b
SuperNova <its.ashwin.23@gmail.com>
2024-10-29 00:07:23 +0530

    footer added to about page

--- app/Company/About/page.tsx
+++ app/Company/About/page.tsx
@@ -4,12 +4,14 @@ import Hero from "./components/HeroSection"
 import Header from "@/app/components/common/Header";
 import OurVision from "./components/OurVision";
 import OurServices from "./components/OurServices";
+import Footer from "@/app/components/common/Footer";
 const AboutPage = () => {
   return <div className={styles.main}>
     <Header/>
     <Hero />
     <OurVision/>
     <OurServices/>
+    <Footer />
   </div>;
 };
 
